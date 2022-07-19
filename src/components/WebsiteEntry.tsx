0d2b79e5e70701d7be5306a6588746dcc4fb666c
Christopher Angelo <angelo@angeloanan.xyz>
2022-07-19 20:20:34 +0700

    feat: init

---
+++ src/components/WebsiteEntry.tsx
@@ -0,0 +1,87 @@
+import { Check, X } from 'react-feather'
+import { SimpleIcon } from 'simple-icons'
+
+interface WebsiteEntryProps {
+  website: SimpleIcon
+  pass?: boolean
+  loading?: boolean
+}
+
+export const WebsiteEntry = ({ website, pass = false, loading = false }: WebsiteEntryProps) => {
+  const size = 32
+
+  // TODO: Refactor
+  if (loading) {
+    return (
+      <div className='flex items-center gap-2 rounded bg-neutral-100 p-4 outline outline-2 outline-neutral-300'>
+        <svg height={size} width={size} fill='currentColor' style={{ color: website.hex }}>
+          <title>{website.title}</title>
+          <path d={website.path} />
+        </svg>
+
+        <div>
+          <h3 className='font-semibold text-neutral-800'>{website.title}</h3>
+          <p className='flex justify-center gap-1'>
+            <svg
+              className='mr-2 h-5 w-5 animate-spin text-neutral-800'
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+            >
+              <circle
+                className='opacity-25'
+                cx='12'
+                cy='12'
+                r='10'
+                stroke='currentColor'
+                strokeWidth='4'
+              ></circle>
+              <path
+                className='opacity-75'
+                fill='currentColor'
+                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+              ></path>
+            </svg>
+            Loading...
+          </p>
+        </div>
+      </div>
+    )
+  }
+
+  if (pass) {
+    return (
+      <div className='flex items-center gap-2 rounded bg-green-100 p-4 outline outline-2 outline-green-400'>
+        <svg height={size} width={size} fill='currentColor' style={{ color: website.hex }}>
+          <title>{website.title}</title>
+          <path d={website.path} />
+        </svg>
+
+        <div>
+          <h3 className='font-semibold text-green-800'>{website.title}</h3>
+          <p className='flex gap-1'>
+            <Check className='text-green-800' /> Pass
+          </p>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className='flex items-center gap-2 rounded bg-red-200 p-4 outline outline-2 outline-red-500'>
+        <div className='flex h-8 w-8 items-center'>
+          <svg height={size} width={size} fill='currentColor' style={{ color: website.hex }}>
+            <title>{website.title}</title>
+            <path d={website.path} />
+          </svg>
+        </div>
+
+        <div>
+          <h3 className='text-lg font-bold text-red-800'>{website.title}</h3>
+          <p className='flex gap-1 font-medium italic text-red-700'>
+            <X className='text-red-600' /> BLOCKED
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,86 @@
+import Fuse from 'fuse.js'
 import * as React from 'react'
+import useSWR from 'swr'
+
+import { websiteList } from '../_data/websites'
+import { WebsiteEntry } from '../components/WebsiteEntry'
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 const IndexPage = () => {
-  return <div>Hello world!</div>
+  const { data, error } = useSWR('/api/fetchEntries', fetcher, {
+    refreshInterval: 60_000,
+    revalidateOnFocus: false
+  })
+
+  const fuseInstance = React.useRef<any>(null)
+  const [searchQuery, setSearchQuery] = React.useState('')
+
+  React.useEffect(() => {
+    if (data == null) return
+
+    fuseInstance.current = new Fuse(data.data, {
+      keys: ['attributes.website'],
+      isCaseSensitive: true,
+      threshold: 0.8
+    })
+  }, [data])
+
+  return (
+    <div className='flex w-full justify-center'>
+      <div className='w-full max-w-screen-xl p-8'>
+        <div className='flex w-full flex-col items-center'>
+          <h1 className='text-5xl font-bold italic'>Kominfo&apos;d?</h1>
+          <span>Website ini bakal keblokir ga ya?</span>
+        </div>
+
+        <div>
+          <h2 className='text-2xl font-semibold'>Top websites</h2>
+          <p>
+            Sumber data website diambil dari{' '}
+            <a
+              href='https://www.semrush.com/blog/most-visited-websites/'
+              className='text-blue-700 underline'
+            >
+              semrush.com
+            </a>
+          </p>
+          <div className='mt-4 grid grid-flow-row grid-cols-4 gap-8'>
+            {websiteList.map((website) => (
+              <WebsiteEntry
+                website={website.icon}
+                key={website.icon.title}
+                loading={data == null}
+                pass={
+                  // TODO: Properly type this
+                  data?.data.filter(
+                    (e: { attributes: { website: string } }) =>
+                      e.attributes.website === website.website
+                  ).length > 0
+                }
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className='mt-8'>
+          <h2 className='text-2xl font-semibold'>Cari manual (in development)</h2>
+          Jika situs tidak tertampil diatas, anda bisa mencari situs tersebut di sini:{' '}
+          <input type='search' onChange={(e) => setSearchQuery(e.target.value)} />
+        </div>
+
+        {data && fuseInstance.current != null && (
+          <>
+            <pre className='overflow-scroll overflow-y-hidden'>
+              {JSON.stringify((fuseInstance.current as Fuse<unknown>).search(searchQuery), null, 2)}
+            </pre>
+          </>
+        )}
+
+        {/* {data && <pre className='overflow-scroll'>{JSON.stringify(data, null, 2)}</pre>} */}
+      </div>
+    </div>
+  )
 }
 
 export default IndexPage
