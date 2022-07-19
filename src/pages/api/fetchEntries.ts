a8da7f380ceddf560b575a11de386a66d53959de
Christopher Angelo <angelo@angeloanan.xyz>
2022-07-20 00:30:47 +0700

    fix(api): actually use updated data

--- src/pages/api/fetchEntries.ts
+++ src/pages/api/fetchEntries.ts
@@ -3,23 +3,23 @@ import { NextApiHandler } from 'next'
 import defaultData from './default.json'
 
 const handler: NextApiHandler = async (req, res) => {
-  return res
-    .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
-    .json(defaultData)
+  // return res
+  //   .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
+  //   .json(defaultData)
 
-  // try {
-  //   const dataFetch = await fetch('https://pse.kominfo.go.id/api/v1/jsonapi/tdpse-terbit')
-  //   const dataJson = await dataFetch.json()
+  try {
+    const dataFetch = await fetch('https://pse.kominfo.go.id/api/v1/jsonapi/tdpse-terbit')
+    const dataJson = await dataFetch.json()
 
-  //   return res
-  //     .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
-  //     .json(dataJson)
-  // } catch {
-  //   // Use default
-  //   return res
-  //     .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
-  //     .json(defaultData)
-  // }
+    return res
+      .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
+      .json(dataJson)
+  } catch {
+    // Use default
+    return res
+      .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
+      .json(defaultData)
+  }
 }
 
 export default handler
