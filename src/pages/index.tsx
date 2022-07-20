5990ee37668c198a04307946e89197dc6961ad32
Christopher Angelo <angelo@angeloanan.xyz>
2022-07-20 11:50:01 +0700

    feat: semantic html and about section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,12 +36,12 @@ const IndexPage = () => {
 
       <div className='flex w-full justify-center'>
         <div className='w-full max-w-screen-xl p-8'>
-          <div className='flex w-full flex-col items-center'>
+          <header className='flex w-full flex-col items-center'>
             <h1 className='text-5xl font-bold italic'>Kominfo&apos;d?</h1>
             <span>Website ini bakal keblokir ga ya?</span>
-          </div>
+          </header>
 
-          <div className='mt-16'>
+          <section className='mt-16'>
             <h2 className='text-2xl font-semibold'>Confused on what this is?</h2>
             <p>The Indonesian government plans to ban &apos;online service providers&apos;.</p>
             <p>
@@ -50,9 +50,9 @@ const IndexPage = () => {
                 <a className='text-blue-700 underline'>kominfu.com</a>
               </Link>
             </p>
-          </div>
+          </section>
 
-          <div className='mt-8'>
+          <section className='mt-8'>
             <h2 className='text-2xl font-semibold'>Indonesia Starterpack</h2>
             <p>Situs atau service ini terpopuler dengan orang Indonesia</p>
             <div className='mt-4 grid grid-flow-row grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
@@ -70,9 +70,9 @@ const IndexPage = () => {
                 />
               ))}
             </div>
-          </div>
+          </section>
 
-          <div className='mt-8'>
+          <section className='mt-8'>
             <h2 className='text-2xl font-semibold'>Top websites USA</h2>
             <p>
               2022 Top websites in the USA. Sumber data website diambil dari{' '}
@@ -103,9 +103,9 @@ const IndexPage = () => {
                 />
               ))}
             </div>
-          </div>
+          </section>
 
-          <div className='mt-8 mb-4'>
+          <section className='mt-8 mb-4'>
             <h2 className='text-2xl font-semibold'>Cari manual</h2>
             Jika situs tidak tertampil diatas, anda bisa mencari situs tersebut di sini:{' '}
             <input
@@ -113,11 +113,12 @@ const IndexPage = () => {
               type='search'
               onChange={(e) => setSearchQuery(e.target.value)}
             />
-          </div>
+          </section>
 
-          {data && fuseInstance.current != null && (
-            <>
-              {(fuseInstance.current as Fuse<unknown>)
+          <section className='pb-8'>
+            {data &&
+              fuseInstance.current != null &&
+              (fuseInstance.current as Fuse<unknown>)
                 .search(debouncedQuery, { limit: 5 })
                 .map((v) => {
                   const item = v.item as { attributes: { nama: string; website: string } }
@@ -133,9 +134,25 @@ const IndexPage = () => {
                     </div>
                   )
                 })}
-            </>
-          )}
-          <footer className='mt-32 text-sm font-light'>
+          </section>
+
+          <section className=''>
+            <h2 className='text-2xl font-semibold'>Bagaimana cara kerja situs ini?</h2>
+            <p className='mt-2 max-w-prose'>
+              Website ini mengambil data dari situs{' '}
+              <Link href='https://pse.kominfo.go.id' passHref>
+                <a className='text-blue-800 underline'>PSE Kominfo</a>
+              </Link>
+              . Data situs ini akan secara otomatis diperbaharui setiap 1 menit. Tetapi karena situs
+              kominfo sangat tidak stabil, mungkin data yang disajikan bukanlah data yang paling
+              terbarui.
+            </p>
+            <p className='mt-2'>
+              Pemilik situs ini akan mencoba untuk menyajikan data yang terbaru dan terakurat.
+            </p>
+          </section>
+
+          <footer className='mt-8 text-sm font-light'>
             <Link href='https://angeloanan.xyz' passHref>
               <a>
                 <div>Created with 🤨, 😠 and 😡 by Angelo!</div>
