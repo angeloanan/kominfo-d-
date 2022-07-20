de43d106ea5af1db5114db6dea9c2f9663af217b
Christopher Angelo <angelo@angeloanan.xyz>
2022-07-20 20:06:56 +0700

    fix(api): monkey patch api

--- src/pages/api/fetchEntries.ts
+++ src/pages/api/fetchEntries.ts
@@ -4,31 +4,31 @@ import { setTimeout } from 'timers/promises'
 import defaultData from './default.json'
 
 const handler: NextApiHandler = async (req, res) => {
-  // return res
-  //   .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
-  //   .json(defaultData)
+  return res
+    .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=900')
+    .json(defaultData)
 
-  try {
-    const race = await Promise.race([
-      setTimeout(8000, null),
-      fetch('https://pse.kominfo.go.id/api/v1/jsonapi/tdpse-terbit')
-    ])
+  // try {
+  //   const race = await Promise.race([
+  //     setTimeout(8000, null),
+  //     fetch('https://pse.kominfo.go.id/api/v1/jsonapi/tdpse-terbit')
+  //   ])
 
-    if (race != null) {
-      return res
-        .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
-        .json(await race.json())
-    } else {
-      return res
-        .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
-        .json(defaultData)
-    }
-  } catch {
-    // Use default
-    return res
-      .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
-      .json(defaultData)
-  }
+  //   if (race != null) {
+  //     return res
+  //       .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
+  //       .json(await race.json())
+  //   } else {
+  //     return res
+  //       .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
+  //       .json(defaultData)
+  //   }
+  // } catch {
+  //   // Use default
+  //   return res
+  //     .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
+  //     .json(defaultData)
+  // }
 }
 
 export default handler
