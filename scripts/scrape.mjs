7dd6a4a4d578ec0bc00f293d3d254019a28c8d03
Christopher Angelo <angelo@angeloanan.xyz>
2022-07-26 02:09:36 +0700

    feat: banyak sih yang ganti, rada panik juga
    * they changed the API
    * moved fallback data from `src/pages/api/default.json` -> `public/data.json`
    * fallback data is now a static file that will be updated once per hour (implementation coming using gh actions)
    * api now serves the static file
    * exported types to an external file
    * added scrape script

---
+++ scripts/scrape.mjs
@@ -0,0 +1,77 @@
+// Run this with --experimental-fetch
+
+import fs from 'fs/promises'
+
+const generateAsingApiPageNumber = (number = 0) => {
+  return `https://pse.kominfo.go.id/static/json-static/ASING_TERDAFTAR/${number}.json`
+}
+
+const generateLokalApiPageNumber = (number = 0) => {
+  return `https://pse.kominfo.go.id/static/json-static/LOKAL_TERDAFTAR/${number}.json`
+}
+
+const [asingInitialReq, lokalInitialReq] = await Promise.all([
+  fetch(generateAsingApiPageNumber()),
+  fetch(generateLokalApiPageNumber())
+])
+const [asingInitialRes, lokalInitialRes] = await Promise.all([
+  asingInitialReq.json(),
+  lokalInitialReq.json()
+])
+const asingApiResponseMeta = asingInitialRes.meta
+const lokalApiResponseMeta = lokalInitialRes.meta
+
+const asingArr = Array.from(Array(asingApiResponseMeta.page.lastPage).keys())
+const lokalArr = Array.from(Array(lokalApiResponseMeta.page.lastPage).keys())
+
+// Removes 0th element because we have it already
+asingArr.shift()
+lokalArr.shift()
+
+console.group(`# Fetching data asing - ${asingArr.length} halaman`)
+console.time('data-asing')
+// TODO: This is still possible since there are only like 80 pages of data here
+// This might cause socket hangups when more companies registers
+const asingResult = await Promise.all(
+  asingArr.map(async (pageNum) => {
+    const url = generateAsingApiPageNumber(pageNum)
+    console.log(`Fetching halaman ${pageNum}`)
+    const fetchReq = await fetch(url)
+    const fetchRes = await fetchReq.json()
+    console.log(`Selesai mengambil halaman ${pageNum}`)
+
+    return fetchRes
+  })
+)
+console.groupEnd()
+console.timeEnd('data-asing')
+
+console.group(`# Fetching data lokal - ${lokalArr.length} halaman`)
+console.time('data-lokal')
+const lokalResult = []
+for await (const pageNum of lokalArr) {
+  const url = generateLokalApiPageNumber(pageNum)
+
+  console.info(`Fetching halaman ${pageNum}`)
+  const fetchReq = await fetch(url)
+  const fetchRes = await fetchReq.json()
+
+  lokalResult.push(fetchRes)
+}
+console.groupEnd()
+console.timeEnd('data-lokal')
+
+// Add back 0th element
+asingResult.unshift(asingInitialRes)
+lokalResult.unshift(lokalInitialRes)
+
+const transposedResult = asingResult
+  .flatMap((p) => p.data)
+  .concat(lokalResult.flatMap((p) => p.data))
+
+console.group('# Final data output')
+console.log(transposedResult)
+console.groupEnd()
+
+console.log('# Writing to file')
+fs.writeFile('../public/data.json', JSON.stringify(transposedResult, null, 2), { encoding: 'utf8' })
