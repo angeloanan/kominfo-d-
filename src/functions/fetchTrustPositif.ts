5eb0673d358dbbe558d3fff7b314e5b39d5993f4
Adrian Ardizza <tmgfaction@gmail.com>
2022-07-31 00:59:12 +0700

    Split TP check request to chunks of 100 websites at a time

--- src/functions/fetchTrustPositif.ts
+++ src/functions/fetchTrustPositif.ts
@@ -14,26 +14,36 @@ const httpsAgent = new https.Agent({
 })
 
 export const fetchTrustPositif = async () => {
-  const trustPositifData: Record<string, boolean> = await fetch(
-    'https://trustpositif.kominfo.go.id/Rest_server/getrecordsname_home',
-    {
-      method: 'POST',
-      body: new URLSearchParams({
-        name: allWebsitesCombined
-          .map((item) => item.website.replace(/(https?:\/\/)/, '').replace(/www\./, ''))
-          .join('\n')
-      }),
-      agent: httpsAgent
-    } as any
+  const requestChunks = Array.from(
+    { length: Math.ceil(allWebsitesCombined.length / 100) },
+    (_, i) => allWebsitesCombined.slice(i * 100, i * 100 + 100)
   )
-    .then((res) => res.json())
-    .then((res) => {
-      let trustPositifStatus: Record<string, boolean> = {}
-      res.values.forEach((item: any) => {
-        trustPositifStatus[item.Domain] = item.Status === 'Ada'
-      })
-      return trustPositifStatus
-    })
 
-  return trustPositifData
+  const requests: Promise<Record<string, boolean>>[] = []
+
+  requestChunks.forEach((chunk) => {
+    requests.push(
+      fetch('https://trustpositif.kominfo.go.id/Rest_server/getrecordsname_home', {
+        method: 'POST',
+        body: new URLSearchParams({
+          name: chunk
+            .map((item) => item.website.replace(/(https?:\/\/)/, '').replace(/www\./, ''))
+            .join('\n')
+        }),
+        agent: httpsAgent
+      } as any)
+        .then((res) => res.json())
+        .then((res) => {
+          let trustPositifStatus: Record<string, boolean> = {}
+          res.values.forEach((item: any) => {
+            trustPositifStatus[item.Domain] = item.Status === 'Ada'
+          })
+          return trustPositifStatus
+        })
+    )
+  })
+
+  const trustPositifData = await Promise.all(requests)
+
+  return Object.assign({}, ...trustPositifData)
 }
