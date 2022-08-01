1090ab17a2d5e644df2dae3ae2c8a9eb59e5368e
Christopher Angelo <angelo@angeloanan.xyz>
2022-08-01 15:17:18 +0700

    fix: await promise

--- src/pages/api/fetchTrustPositif.ts
+++ src/pages/api/fetchTrustPositif.ts
@@ -3,7 +3,7 @@ import { NextApiHandler } from 'next'
 import { fetchTrustPositif } from '../../functions/fetchTrustPositif'
 
 const handler: NextApiHandler = async (req, res) => {
-  const trustPositifData = fetchTrustPositif()
+  const trustPositifData = await fetchTrustPositif()
 
   return res
     .setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=3600')
