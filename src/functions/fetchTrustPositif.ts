8dd888db3b7adf8be2bc1dbc91279486096b352c
Christopher Angelo <angelo@angeloanan.xyz>
2022-08-01 15:35:29 +0700

    feat: implement basic trycatching

--- src/functions/fetchTrustPositif.ts
+++ src/functions/fetchTrustPositif.ts
@@ -14,36 +14,43 @@ const httpsAgent = new https.Agent({
 })
 
 export const fetchTrustPositif = async () => {
-  const requestChunks = Array.from(
-    { length: Math.ceil(allWebsitesCombined.length / 100) },
-    (_, i) => allWebsitesCombined.slice(i * 100, i * 100 + 100)
-  )
-
-  const requests: Promise<Record<string, boolean>>[] = []
-
-  requestChunks.forEach((chunk) => {
-    requests.push(
-      fetch('https://trustpositif.kominfo.go.id/Rest_server/getrecordsname_home', {
-        method: 'POST',
-        body: new URLSearchParams({
-          name: chunk
-            .map((item) => item.website.replace(/(https?:\/\/)/, '').replace(/www\./, ''))
-            .join('\n')
-        }),
-        agent: httpsAgent
-      } as any)
-        .then((res) => res.json())
-        .then((res) => {
-          let trustPositifStatus: Record<string, boolean> = {}
-          res.values.forEach((item: any) => {
-            trustPositifStatus[item.Domain] = item.Status === 'Ada'
-          })
-          return trustPositifStatus
-        })
+  try {
+    const requestChunks = Array.from(
+      { length: Math.ceil(allWebsitesCombined.length / 100) },
+      (_, i) => allWebsitesCombined.slice(i * 100, i * 100 + 100)
     )
-  })
 
-  const trustPositifData = await Promise.all(requests)
+    const requests: Promise<Record<string, boolean>>[] = []
 
-  return Object.assign({}, ...trustPositifData)
+    requestChunks.forEach((chunk) => {
+      requests.push(
+        fetch('https://trustpositif.kominfo.go.id/Rest_server/getrecordsname_home', {
+          method: 'POST',
+          header: {},
+          body: new URLSearchParams({
+            name: chunk
+              .map((item) => item.website.replace(/(https?:\/\/)/, '').replace(/www\./, ''))
+              .join('\n')
+          }),
+          agent: httpsAgent
+        } as any)
+          .then((res) => res.json())
+          .then((res) => {
+            let trustPositifStatus: Record<string, boolean> = {}
+            res.values.forEach((item: any) => {
+              trustPositifStatus[item.Domain] = item.Status === 'Ada'
+            })
+            return trustPositifStatus
+          })
+      )
+    })
+
+    const trustPositifData = await Promise.all(requests)
+
+    return Object.assign({}, ...trustPositifData)
+  } catch (e) {
+    console.error(e)
+
+    return {}
+  }
 }
