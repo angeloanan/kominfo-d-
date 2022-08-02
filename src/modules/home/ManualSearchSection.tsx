5aa3146135fb06ca675f11c525fae8108fb25ece
arcetros <75669794+arcetros@users.noreply.github.com>
2022-08-02 18:07:22 +0800

    feat: minor tweaks for manual search

--- src/modules/home/ManualSearchSection.tsx
+++ src/modules/home/ManualSearchSection.tsx
@@ -1,3 +1,4 @@
+import { SearchIcon } from '@heroicons/react/solid'
 import type Fuse from 'fuse.js'
 import Link from 'next/link'
 import * as React from 'react'
@@ -28,49 +29,72 @@ export const ManualSearchSection = () => {
 
   return (
     <>
-      <section className='mt-8 mb-4'>
-        <h2 className='text-2xl font-semibold'>Cari manual</h2>
-        Jika situs tidak tertampil diatas, anda bisa mencari situs tersebut di sini:{' '}
-        <input
-          className='rounded border border-black p-2'
-          type='search'
-          onChange={async (e) => {
-            // Fetch fuse on first input
-            if (FullPSEData.current == null) {
-              FullPSEData.current = await fetch('/data.json').then((res) => res.json())
-              if (fuseInstance.current == null) {
-                Fuse.current = (await import('fuse.js')).default
+      <section className='mt-8 w-full rounded bg-brand-bg shadow'>
+        <div className='relative flex flex-col justify-between lg:flex-row'>
+          <div className='w-fit p-4'>
+            <h2 className='text-2xl font-semibold'>Cari manual</h2>
+            <p className='mt-1 text-gray-500'>
+              Jika situs tidak tertampil diatas, anda bisa mencari situs tersebut di sini
+            </p>
+          </div>
+          <div className='relative w-full lg:w-1/2'>
+            <SearchIcon className='absolute top-1/2 left-4 h-5 w-5 -translate-y-1/2 transform text-gray-400 md:h-8 md:w-8' />
+            <input
+              className='h-full w-full border-l-0 border-gray-400 bg-gray-50 py-4 px-12 text-lg font-semibold text-gray-600 outline-none md:border-l md:px-16'
+              type='search'
+              placeholder='Cari Website'
+              onChange={async (e: { target: { value: any } }) => {
+                // Fetch fuse on first input
+                if (FullPSEData.current == null) {
+                  FullPSEData.current = await fetch('/data.json').then((res) => res.json())
+                  if (fuseInstance.current == null) {
+                    Fuse.current = (await import('fuse.js')).default
 
-                fuseInstance.current = new Fuse.current(FullPSEData.current!, {
-                  keys: ['attributes.website'],
-                  isCaseSensitive: true,
-                  threshold: 0.8
-                })
-              }
-            }
+                    fuseInstance.current = new Fuse.current(FullPSEData.current!, {
+                      keys: ['attributes.website'],
+                      isCaseSensitive: true,
+                      threshold: 0.8
+                    })
+                  }
+                }
 
-            setSearchQuery(e.target.value)
-          }}
-        />
+                setSearchQuery(e.target.value)
+              }}
+            />
+          </div>
+        </div>
       </section>
-      <section className='pb-8'>
+      <ul className='mt-1'>
         {FullPSEData.current != null &&
           fuseInstance != null &&
           (fuseInstance.current as Fuse<PSEData>)?.search(debouncedQuery, { limit: 5 }).map((v) => {
             const item = v.item
 
             return (
-              <div key={v.refIndex} className='italic'>
+              <li
+                key={v.refIndex}
+                className='group border-y border-gray-50 bg-white p-4 shadow first:rounded-t last:rounded-b hover:bg-slate-50'
+              >
                 <Link href={`https://${item.attributes.website}`} passHref>
-                  <a className='text-blue-800 underline'>
-                    <span className='font-bold'>{item.attributes.nama}</span> (
-                    {item.attributes.website}) | Registered by {item.attributes.nama_perusahaan}
+                  <a className='items-left flex flex-col justify-between md:flex-row md:items-center'>
+                    <div className='flex flex-col flex-nowrap font-bold text-gray-700'>
+                      {item.attributes.nama}
+                      <span className='font-normal text-blue-500 group-hover:underline'>
+                        {item.attributes.website.length > 50
+                          ? `${item.attributes.website.substring(0, 50)} ...`
+                          : item.attributes.website}
+                      </span>
+                    </div>
+                    <span className='mt-4 text-sm md:mt-0'>
+                      Registered by{' '}
+                      <span className='underline'>{item.attributes.nama_perusahaan}</span>
+                    </span>
                   </a>
                 </Link>
-              </div>
+              </li>
             )
           })}
-      </section>
+      </ul>
     </>
   )
 }
